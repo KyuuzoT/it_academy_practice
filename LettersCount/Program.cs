09320c0bb965c9da93394e56fcc541544c3d2c74
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-08 20:59:06 +0300

    Task2 - done;

---
+++ LettersCount/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LettersCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo again;
+            do
+            {
+                CountLettersInWord();
+                Console.WriteLine("Again (Y,N)?");
+                again = Console.ReadKey();
+                Console.WriteLine();
+            } while (again.Key != ConsoleKey.N);
+            Console.ReadKey();
+        }
+
+        private static void CountLettersInWord()
+        {
+            Console.WriteLine("Input any word: ");
+            string input = Console.ReadLine();
+            int lettersCount = 0;
+            foreach (var s in input)
+            {
+                if (s == 'a' || s.Equals('A'))
+                {
+                    lettersCount++;
+                }
+            }
+
+            Console.WriteLine($"Number of letters in word {input} equals to {lettersCount}");
+        }
+    }
+}
