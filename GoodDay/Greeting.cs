231316820934f7e3758f768da272081237093ce1
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-08 20:53:36 +0300

    Task1 - done

---
+++ GoodDay/Greeting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodDay
+{
+    class Greeting
+    {
+        public static string GetTimeOfDay()
+        {
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+            string timeOfDay;
+            switch (currentTime.Hours)
+            {
+                case int hour when (hour >= 9 && hour < 12):
+                    return timeOfDay = "morning";
+                case int hour when (hour >= 12 && hour < 15):
+                    return timeOfDay = "day";
+                case int hour when (hour >= 15 && hour <= 22):
+                    return timeOfDay = "evening";
+                default:
+                    return "Unresolved time";
+            }
+        }
+
+        public Greeting()
+        {
+            Console.WriteLine($"Good {GetTimeOfDay()}, guys!");
+        }
+    }
+}
---
+++ GoodDay/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo again;
+            do
+            {
+                Greeting greeting = new Greeting();
+                Console.WriteLine("Again (Y,N)?");
+                again = Console.ReadKey();
+                Console.WriteLine();
+            } while (again.Key != ConsoleKey.N);
+        }
+    }
+}
