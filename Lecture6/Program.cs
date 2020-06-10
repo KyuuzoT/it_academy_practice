ca3b9922485deb6bcfa660584e5adae36d8074a4
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-06-10 20:50:37 +0300

    Task 1 - done

--- Lecture6/Program.cs
+++ Lecture6/Program.cs
@@ -6,7 +6,29 @@ namespace Lecture6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            emptyArray();
+            arrayWithThreeDifferentElems();
+            arrayPrintFindMax();
+            boxingUnboxing();
+
+            Console.ReadKey();
+        }
+
+        private static void emptyArray()
+        {
+            int[] array = new int[0];//Array contains zero elements - is it empty?
+        }
+
+        private static void arrayWithThreeDifferentElems()
+        {
+        }
+
+        private static void arrayPrintFindMax()
+        {
+        }
+
+        private static void boxingUnboxing()
+        {
         }
     }
 }
