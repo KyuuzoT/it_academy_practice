abc4e3a2de2977d946bfb4c2caad4614f44d51eb
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-07-13 19:37:19 +0300

    Task2 - done

--- Lecture 14/Task 14-2/Program.cs
+++ Lecture 14/Task 14-2/Program.cs
@@ -4,9 +4,18 @@ namespace Task_14_2
 {
     class Program
     {
+        private static string _inputString = "Hello, guys!";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string result;
+            result = _inputString.TrimString(0, 5);
+            Console.WriteLine(result);
+            result = _inputString.TrimString(5, 9);
+            Console.WriteLine(result);
+
+            Console.ReadKey();
         }
+
+
     }
 }
---
+++ Lecture 14/Task 14-2/StringExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_14_2
+{
+    static class StringExtension
+    {
+        public static string TrimString(this String str, int numberOfLetters)
+        {
+            return $"{str.Substring(0, numberOfLetters)}...";
+        }
+
+        public static string TrimString(this String str, int start, int end)
+        {
+            string trimmed = String.Empty;
+            for (int i = 0; i < end - start; i++)
+            {
+                trimmed += str[i + start];
+            }
+
+            if(start > 0)
+            {
+                return $"...{trimmed}...";
+            }
+            return $"{trimmed}...";
+        }
+    }
+}
