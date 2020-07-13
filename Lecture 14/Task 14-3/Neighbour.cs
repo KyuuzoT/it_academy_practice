5d56e2ca5ccfe8836d4616a177c842c577d56f6c
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-07-13 20:06:18 +0300

    Task3 - done;

---
+++ Lecture 14/Task 14-3/Neighbour.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_14_3
+{
+    class Neighbour
+    {
+        private long _phoneNumber;
+        private short _flatNumber;
+        private string _fullName;     
+
+        public long PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                if(value < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Value cannot be less then 0");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    _phoneNumber = value;
+                }
+            }
+        }
+
+        public short FlatNumber
+        {
+            get => _flatNumber;
+            set
+            {
+                if (value < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Value cannot be less then 0");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    _flatNumber = value;
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get => _fullName;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Value cannot be empty");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    _fullName = value;
+                }
+            }
+        }
+
+        public string GetNeighbourInfo()
+        {
+            return $"Your neighbour {_fullName} from appartment {_flatNumber} with tel. number {_phoneNumber}";
+        }
+    }
+}
--- Lecture 14/Task 14-3/Program.cs
+++ Lecture 14/Task 14-3/Program.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Task_14_3
 {
     class Program
     {
+        private static int _flatRndMin = 1;
+        private static int _flatRndMax = 25;
+        private static int _phoneRndMin = 8800555;
+        private static int _phoneRndMax = int.MaxValue;
+        private static int _quantityOfNeighbours = 20;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IList<Neighbour> floorNeighbours = new List<Neighbour>();
+            AddFakeNeighboursInList(floorNeighbours);
+            PrintList(floorNeighbours);
+            FindNeighbourWithExactFlat(floorNeighbours);
+
+            Console.ReadKey();
+        }
+
+        private static void AddFakeNeighboursInList(IList<Neighbour> floorNeighbours)
+        {
+            Neighbour neighbour;
+            for (int i = 0; i < _quantityOfNeighbours; i++)
+            {
+                neighbour = new Neighbour();
+                neighbour.FlatNumber = (short)(new Random().Next(_flatRndMin, _flatRndMax));
+                neighbour.PhoneNumber = new Random().Next(_phoneRndMin, _phoneRndMax);
+                neighbour.FullName = Char.ConvertFromUtf32(new Random().Next(0x0041, 0x007A));
+                floorNeighbours.Add(neighbour);
+            }
+        }
+
+        private static void FindNeighbourWithExactFlat(IList<Neighbour> floorNeighbours)
+        {
+            short flatNumber = 0;
+            Console.WriteLine("Please, input flat number you wish to look up: ");
+            if(short.TryParse(Console.ReadLine(), out flatNumber))
+            {
+                foreach (var item in floorNeighbours)
+                {
+                    if(item.FlatNumber.Equals(flatNumber))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Neighbour was found!");
+                        Console.ResetColor();
+                        Console.WriteLine(item.GetNeighbourInfo());
+                    }
+                }
+            }
+        }
+
+        private static void PrintList(IList<Neighbour> floorNeighbours)
+        {
+            foreach (var item in floorNeighbours)
+            {
+                Console.WriteLine(item.GetNeighbourInfo());
+            }
         }
     }
 }
