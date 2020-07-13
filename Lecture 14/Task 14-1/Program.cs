9c3c85329ad9cefb074be4cd657b3ce861acba26
Роман Окатов Kyuuzo <kyuuzo@tut.by>
2020-07-13 19:23:59 +0300

    Task1 - done

--- Lecture 14/Task 14-1/Program.cs
+++ Lecture 14/Task 14-1/Program.cs
@@ -4,9 +4,59 @@ namespace Task_14_1
 {
     class Program
     {
+        private static int rndMin = 1;
+        private static int rndMax = 4;
+        private static int _arraySize = 10;
+        private static Rectangle[] _rectangles;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _rectangles = AddRectanglesToArray();
+            PrintArray(_rectangles);
+            FindDuplicates(_rectangles);
+
+            Console.ReadKey();
+        }
+
+        private static Rectangle[] AddRectanglesToArray()
+        {
+            Rectangle[] rects = new Rectangle[_arraySize];
+            Rectangle item;
+
+            for (int i = 0; i < rects.Length; i++)
+            {
+                item = new Rectangle();
+                item.Width = new Random().Next(rndMin, rndMax);
+                item.Height = new Random().Next(rndMin, rndMax);
+                rects[i] = item;
+            }
+
+            return rects;
+        }
+
+        private static void FindDuplicates(Rectangle[] rectangles)
+        {
+            int dupsQuantity = 0;
+            foreach (var item in rectangles)
+            {
+                if(item.Height.Equals(rectangles[0].Height) && item.Width.Equals(rectangles[0].Width))
+                {
+                    Console.WriteLine("Duplicate found!");
+                    dupsQuantity++;
+                }
+            }
+
+            Console.WriteLine($"Quantity of duplicates: {dupsQuantity}");
+        }
+
+        private static void PrintArray(Rectangle[] rectangles)
+        {
+            int indexer = 0;
+            foreach (var item in rectangles)
+            {
+                Console.WriteLine($"{indexer}. {item.GetRectangleInfo()}");
+                indexer++;
+            }
         }
     }
 }
---
+++ Lecture 14/Task 14-1/Rectangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_14_1
+{
+    struct Rectangle
+    {
+        private int _width;
+        private int _height;
+
+        public int Width
+        {
+            get => _width;
+            set
+            {
+                addValue(value, out _width);
+            }
+        }
+
+        public int Height
+        {
+            get => _height;
+            set
+            {
+                addValue(value, out _height);
+            }
+        }
+
+        private void addValue(int inputValue, out int outputValue)
+        {
+            if (inputValue > 0)
+            {
+                outputValue = inputValue;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Value cannot be less then 0");
+                Console.ResetColor();
+                outputValue = 0;
+            }
+        }
+
+        public string GetRectangleInfo()
+        {
+            return $"Rectangle with height: {_height} and width {_width}.";
+        }
+    }
+}
